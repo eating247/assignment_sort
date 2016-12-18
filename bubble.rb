c3e97b41ffa8e1ff17448bf2cf4e984190ce7733
tt <ttwang.303@gmail.com>
2016-12-17 23:03:59 -0500

    finish bubble

--- bubble.rb
+++ bubble.rb
@@ -1,18 +1,16 @@
-def bubble_sort(array)
-    first = array[0]
-    second = array[1]
-  until array == array.sort
-    if first > second
-      first,second = second,first
-    end
+require 'pry'
 
-    if second = array.last
-      first = array[0]
-      second = array[1]
-    else
-      first = first.next
-      second = second.next
+def bubble_sort(array)
+  last = array.length - 2
+  
+  array.length.times do
+    (0..last).each do |index|
+      first = array[index]
+      second = array[index+1] 
+      if first > second 
+        array[index], array[index+1] = second, first
+      end
     end
   end
-
+  array
 end
--- merge.rb
+++ merge.rb
@@ -1,3 +1,32 @@
 def merge_sort(array)
   array.slice(2)
 end
+
+def bubble_sort(array)
+  last = array.index(array[-2])
+  
+  1000.times do
+    (0..array.length).each do |index|
+      first = array[index]
+      second = array[index+1]
+      if first > second
+        array[index], array[index+1] = second, first
+      end
+    end
+  end
+  array
+end
+
+def bubble_sort(array)
+  array.length.times do
+    array.each_index do |index|
+      first = array[index]
+      second = array[index+1]
+      if first > second
+        array[index], array[index+1] = second, first
+      end
+      end
+    end
+  end
+  array
+end
