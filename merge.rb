e83f53c12e1f4a94465384225299ee32729daa06
tt <ttwang.303@gmail.com>
2016-12-18 00:12:25 -0500

    finish merge

--- merge.rb
+++ merge.rb
@@ -1,32 +1,31 @@
 def merge_sort(array)
-  array.slice(2)
+  return array if array.length <= 1 
+
+  split = array.length / 2
+  left = array[0..split-1]
+  right = array[split..-1]
+
+  left_merge = merge_sort(left)
+  right_merge = merge_sort(right)
+
+  merge(left_merge, right_merge)
+
 end
 
-def bubble_sort(array)
-  last = array.index(array[-2])
-  
-  1000.times do
-    (0..array.length).each do |index|
-      first = array[index]
-      second = array[index+1]
-      if first > second
-        array[index], array[index+1] = second, first
-      end
+def merge(left_merge, right_merge)
+  sorted = []
+  until left_merge.empty? && right_merge.empty?
+    if left_merge.empty?
+      sorted += right_merge
+      right_merge = []
+    elsif right_merge.empty?
+      sorted += left_merge
+      left_merge = []
+    elsif left_merge[0] <= right_merge[0]
+      sorted << left_merge.shift
+    elsif right_merge[0] < left_merge[0]
+      sorted << right_merge.shift
     end
   end
-  array
-end
-
-def bubble_sort(array)
-  array.length.times do
-    array.each_index do |index|
-      first = array[index]
-      second = array[index+1]
-      if first > second
-        array[index], array[index+1] = second, first
-      end
-      end
-    end
-  end
-  array
+  sorted
 end
